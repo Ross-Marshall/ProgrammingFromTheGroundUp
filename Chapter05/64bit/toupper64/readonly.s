7ae2347ac04f66ad422530d72b9567831e57978a
Ross-Marshall <rossmarshallgithub@gmail.com>
2018-07-16 22:13:03 -0400

    readonly

---
+++ Chapter05/64bit/toupper64/readonly.s
@@ -0,0 +1,73 @@
+	.file	"readonly.c"
+	.section	.rodata
+.LC0:
+	.string	"r"
+.LC1:
+	.string	"/tmp/test.txt"
+.LC2:
+	.string	"%s"
+.LC3:
+	.string	"1 : %s\n"
+.LC4:
+	.string	"2: %s\n"
+.LC5:
+	.string	"3: %s\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$272, %rsp
+	leaq	.LC0(%rip), %rsi
+	leaq	.LC1(%rip), %rdi
+	call	fopen@PLT
+	movq	%rax, -8(%rbp)
+	leaq	-272(%rbp), %rdx
+	movq	-8(%rbp), %rax
+	leaq	.LC2(%rip), %rsi
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	__isoc99_fscanf@PLT
+	leaq	-272(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC3(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movq	-8(%rbp), %rdx
+	leaq	-272(%rbp), %rax
+	movl	$255, %esi
+	movq	%rax, %rdi
+	call	fgets@PLT
+	leaq	-272(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC4(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movq	-8(%rbp), %rdx
+	leaq	-272(%rbp), %rax
+	movl	$255, %esi
+	movq	%rax, %rdi
+	call	fgets@PLT
+	leaq	-272(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC5(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movq	-8(%rbp), %rax
+	movq	%rax, %rdi
+	call	fclose@PLT
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Debian 6.3.0-18+deb9u1) 6.3.0 20170516"
+	.section	.note.GNU-stack,"",@progbits
